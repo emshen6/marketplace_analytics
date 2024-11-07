f28aee1f201bcf3a4895b5305bc3fdc45c73700e
emshen6 <parfenovao752@gmail.com>
2024-11-07 03:25:00 +0300

    db_init_final

--- db_init.py
+++ db_init.py
@@ -54,44 +54,112 @@ def check_data_available(date):
 def find_earliest_available_date():
     """Использует бинарный поиск для нахождения самой ранней доступной даты с данными."""
 
-    # Устанавливаем даты без времени
     end_date = pd.to_datetime("today").normalize()
-    start_date = (end_date - timedelta(days=365 * 5)).normalize()  # Например, пять лет назад
+    start_date = (end_date - timedelta(days=365 * 5)).normalize()
 
     print(f'start date = {start_date}')
     print(f'end date = {end_date}')
 
     while start_date < end_date:
         middle_date = start_date + (end_date - start_date) // 2
-        middle_date = middle_date.normalize()  # Округляем до дня
+        middle_date = middle_date.normalize()
 
         if check_data_available(middle_date):
-            print(f'Влево! end date = {end_date}')
-            end_date = middle_date  # Данные есть, ищем в более ранних датах
+            end_date = middle_date
         else:
-            print(f'Вправо! start date = {start_date}')
-            start_date = middle_date + timedelta(days=1)  # Данных нет, ищем в более поздних датах
+            start_date = middle_date + timedelta(days=1)
 
     logging.info(f"Earliest available data found on: {start_date}")
     return start_date
 
-# Находим самую раннюю доступную дату с данными
-#earliest_date = find_earliest_available_date()
+def fill_database(date):
 
-earliest_date = pd.to_datetime("2021-12-31")
-print(check_data_available(earliest_date))
+    params = {
+        "date": date.strftime("%Y-%m-%d")
+    }
 
-params = {
-    "date": earliest_date.strftime("%Y-%m-%d")
-}
+    try:
+        response = requests.get(API_URL, params=params)
 
-try:
-    response = requests.get(API_URL, params=params)
-    if response.status_code == 200:
-        with open(DATA_PATH, "w") as json_file:
-            json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
-        logging.info("Data loaded successfully.")
-    else:
-        logging.error(f"Error {response.status_code}: {response.text}")
-except requests.exceptions.RequestException as e:
-    logging.error(f"Connection error: {e}")
+        if response.status_code == 200:
+            with open(DATA_PATH, "w") as json_file:
+                json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
+            logging.info("Data loaded successfully.")
+        else:
+            logging.error(f"Error {response.status_code}: {response.text}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Connection error: {e}")
+
+    try:
+        with open(DATA_PATH, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            df = pd.DataFrame(data)
+        logging.info("Data successfully loaded from JSON file.")
+    except Exception as e:
+        logging.error(f"Error loading data from JSON: {e}")
+        raise
+
+    try:
+        os.remove(DATA_PATH)
+        logging.info(f"File {DATA_PATH} successfully deleted.")
+    except Exception as e:
+        logging.error(f"Error deleting file {DATA_PATH}: {e}")
+
+    try:
+        df['purchase_datetime'] = pd.to_datetime(df['purchase_datetime']).dt.date
+        df['purchase_time'] = (pd.to_datetime('00:00:00', format='%H:%M:%S') + 
+                            pd.to_timedelta(df['purchase_time_as_seconds_from_midnight'], unit='s')).dt.time
+        logging.info("Data successfully transformed.")
+    except Exception as e:
+        logging.error(f"Error transforming data: {e}")
+        raise
+
+    clients_df = df[['client_id', 'gender']].drop_duplicates().sort_values(by='client_id')
+    products_df = df[['product_id', 'price_per_item', 'discount_per_item']].drop_duplicates().sort_values(by='product_id')
+    df = df.sort_values(by='purchase_time')
+
+    try:
+        database = PGDatabase(
+            host=DATABASE_CREDS["HOST"],
+            database=DATABASE_CREDS["DATABASE"],
+            user=DATABASE_CREDS["USER"],
+            password=DATABASE_CREDS["PASSWORD"],
+        )
+        logging.info("Successfully connected to the database.")
+    except Exception as e:
+        logging.error(f"Error connecting to the database: {e}")
+        raise
+
+    try:
+        for i, row in clients_df.iterrows():
+            query = f"INSERT INTO clients (client_id, gender) VALUES ({row['client_id']}, '{row['gender']}')"
+            database.post(query)
+        logging.info("Client data successfully inserted into the table.")
+    except Exception as e:
+        logging.error(f"Error inserting client data: {e}")
+
+    try:
+        for i, row in products_df.iterrows():
+            query = f"INSERT INTO products (product_id, price_per_item, discount_per_item) VALUES ({row['product_id']}, {row['price_per_item']}, {row['discount_per_item']})"
+            database.post(query)
+        logging.info("Product data successfully inserted into the table.")
+    except Exception as e:
+        logging.error(f"Error inserting product data: {e}")
+
+    try:
+        for i, row in df.iterrows():
+            query = f"INSERT INTO purchases (client_id, product_id, purchase_datetime, purchase_time, quantity, total_price) VALUES ({row['client_id']}, {row['product_id']}, '{row['purchase_datetime']}', '{row['purchase_time']}', {row['quantity']}, {row['total_price']})"
+            database.post(query)
+        logging.info("Purchase data successfully inserted into the table.")
+    except Exception as e:
+        logging.error(f"Error inserting purchase data: {e}")
+
+
+earliest_date = find_earliest_available_date()
+today = pd.to_datetime("today").normalize()
+
+delta = today - earliest_date
+
+for i in range(delta.days + 1):
+    day = earliest_date + timedelta(days=i)
+    fill_database(day)
